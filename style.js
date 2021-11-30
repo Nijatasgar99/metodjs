98c6aaa027d2db946ef31d9fccf6fd3cc2d519ae
Nijatasgar99 <91114183+Nijatasgar99@users.noreply.github.com>
2021-11-30 04:26:20 +0400

    Add files via upload

---
+++ style.js
@@ -0,0 +1,33 @@
+//second task 1 map
+// function myFunction(arr) {
+//     for (let i =0;i<arr.length; i++){
+//         arr[i]*=2
+//     }
+//     return arr;
+// } console.log(myFunction([1,3,6,8,5]));
+
+//second task 2 Reduce ;
+// let sum=0
+// function myFunction(arr){for(let i=0;i<arr.length;i++){sum+=arr[i]}return sum;}
+// console.log(myFunction([1,3,6,8,5]));
+
+//second task 3 filter
+// let arr=[10,11,12,13,40,22,54,60];
+// arr.forEach(num=>{if(num>20){console.log(num);}});
+
+//second task 4 every
+// function myFunction(arr){for(let i=0;i<arr.length;i++){if(arr[i]>15){return true;}else{return false;}}}
+// console.log(myFunction([25,20,36,76,55,40,19]));
+
+//second tas find
+// function myFunction(arr){arr.forEach(num=>{if(num>15){return num; break;}});}
+// console.log(myFunction([18,20,22,30,46,70]));
+//second task foreach
+// const letters=[1,2,4,5,8,];let count={};letters.forEach(item=>{if(count[item]){count[item]++;}else{count[item]=1;}});
+// console.log(count);
+
+//one task
+// const strReverse=str =>str.split('').reverse().join('');
+// console.log(strReverse('hello world'));
+
+
